3f7dcaa3bc77116c6d6eaf8c10d45e4be52bbfc7
Lucas Reade <lucas.reade@yahoo.com>
2021-06-03 11:20:11 +0100

    Add project files.

---
+++ ToDoTables/Models/Tasks.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoTables.Models
+{
+    public class Tasks
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public bool Complete { get; set; }
+        public DateTime Date { get; set; }
+        public string TaskType { get; set; }
+    }
+}
